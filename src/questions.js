b59d5b449b61dd9836b2b623cd8fcbb88de35286
Elvin Guzman <edgd900@gmail.com>
2021-08-19 16:10:30 -0400

    First commit

---
+++ src/questions.js
@@ -0,0 +1,182 @@
+export const questions = [
+    {
+        questionText: '¿Cuál es la capital de los Estados Unidos?',
+        answerOptions: [
+            { answerText: 'New York', isCorrect: false },
+            { answerText: 'Miami', isCorrect: false },
+            { answerText: 'Washington D. C.', isCorrect: true },
+            { answerText: 'Los Angeles', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Isaac Newton es un famoso...',
+        answerOptions: [
+            { answerText: 'Cantante Estadounidense', isCorrect: false },
+            { answerText: 'Físico Ingles', isCorrect: true },
+            { answerText: 'Novelista Alemán', isCorrect: false },
+            { answerText: 'Químico Británico', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿En qué fecha de cada año ocurre el solsticio de verano?',
+        answerOptions: [
+            { answerText: '21 de Julio', isCorrect: false },
+            { answerText: '21 de Enero', isCorrect: false },
+            { answerText: '21 de Febrero', isCorrect: false },
+            { answerText: '21 de Junio', isCorrect: true },
+        ],
+    },
+    {
+        questionText: '¿Cómo se llama la tortuga mascota de Mafalda?',
+        answerOptions: [
+            { answerText: 'Manuelita', isCorrect: false },
+            { answerText: 'Burocracia', isCorrect: true },
+            { answerText: 'Democracia', isCorrect: false },
+            { answerText: "D'Artagnam", isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿Cuál no es un color primario?',
+        answerOptions: [
+            { answerText: 'Rojo', isCorrect: false },
+            { answerText: 'Verde', isCorrect: true },
+            { answerText: 'Amarillo', isCorrect: false },
+            { answerText: 'Azul', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿En qué continente se encuentra Inglaterra?',
+        answerOptions: [
+            { answerText: 'Europa', isCorrect: true },
+            { answerText: 'America', isCorrect: false },
+            { answerText: 'Africa', isCorrect: false },
+            { answerText: 'Asia', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Maria Sharapova es una famosa jugadora en...',
+        answerOptions: [
+            { answerText: 'Gimnasta', isCorrect: false },
+            { answerText: 'Jugadora de baloncesto', isCorrect: false },
+            { answerText: 'Nadadora', isCorrect: false },
+            { answerText: 'Jugadora de tenis', isCorrect: true },
+        ],
+    },
+    {
+        questionText: '¿Cuál es un sustantivo?',
+        answerOptions: [
+            { answerText: 'Partir', isCorrect: false },
+            { answerText: 'Mentir', isCorrect: false },
+            { answerText: 'Cordón', isCorrect: true },
+            { answerText: 'Acordar', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿Cuánto dura la gestación de los elefantes?',
+        answerOptions: [
+            { answerText: '15 meses', isCorrect: false },
+            { answerText: '12 meses', isCorrect: false },
+            { answerText: '9 meses', isCorrect: false },
+            { answerText: '22 meses', isCorrect: true },
+        ],
+    },
+    {
+        questionText: '¿Cuál es el primer nombre del famoso pintor Picasso?',
+        answerOptions: [
+            { answerText: 'Charles', isCorrect: false },
+            { answerText: 'Pablo', isCorrect: true },
+            { answerText: 'Vincent', isCorrect: false },
+            { answerText: 'Adam', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿En qué año murió el ex caudillo Franco?',
+        answerOptions: [
+            { answerText: '20/11/1975', isCorrect: true },
+            { answerText: '20/11/1972', isCorrect: false },
+            { answerText: '20/11/1973', isCorrect: false },
+            { answerText: '20/11/1977', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'La energia del viento se denomina...',
+        answerOptions: [
+            { answerText: 'Energia eólica', isCorrect: true },
+            { answerText: 'Energia térmica', isCorrect: false },
+            { answerText: 'Energia eléctrica', isCorrect: false },
+            { answerText: 'Energia solar', isCorrect: false },
+        ],
+    },
+    {
+        questionText: "¿Quién escribió 'Don Quijote de la Mancha'?",
+        answerOptions: [
+            { answerText: 'Juan Bosh', isCorrect: false },
+            { answerText: 'Manuel Rueda', isCorrect: false },
+            { answerText: 'Miguel de Cervantes', isCorrect: true },
+            { answerText: 'Gabriel García Márquez', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿En qué país se encuentra la isla de Ibiza?',
+        answerOptions: [
+            { answerText: 'Italia', isCorrect: false },
+            { answerText: 'Portugal', isCorrect: false },
+            { answerText: 'Francia', isCorrect: false },
+            { answerText: 'España', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'El esternocleidomastoideo está en una parte del cuerpo cuya función es:',
+        answerOptions: [
+            { answerText: 'Proteger los grandes vasos del cuello', isCorrect: false },
+            { answerText: 'Mover las piernas', isCorrect: false },
+            { answerText: 'Abrir la boca', isCorrect: false },
+            { answerText: 'Girar la cabeza', isCorrect: true },
+        ],
+    },
+    {
+        questionText: '¿En qué año finalizó la segunda guerra mundial?',
+        answerOptions: [
+            { answerText: '1945', isCorrect: true },
+            { answerText: '1950', isCorrect: false },
+            { answerText: '1939', isCorrect: false },
+            { answerText: '1943', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿Cuál es la torre más alta del mundo?',
+        answerOptions: [
+            { answerText: 'Torre PwC', isCorrect: false },
+            { answerText: 'Torre Sevilla', isCorrect: false },
+            { answerText: 'Burj Khalifa', isCorrect: true },
+            { answerText: 'Cepsa', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿Cuál es el punto más profundo del mar de la Tierra?',
+        answerOptions: [
+            { answerText: 'Abismo Challenger', isCorrect: true },
+            { answerText: 'Abismo Brownson', isCorrect: false },
+            { answerText: 'Abismo Molloy', isCorrect: false },
+            { answerText: 'Abismo Factorian', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'La más antigua de las  7 maravillas del mundo es',
+        answerOptions: [
+            { answerText: 'Estatua de Zeus', isCorrect: false },
+            { answerText: 'Templo de Artemisa', isCorrect: false },
+            { answerText: 'Pirámides de Giza', isCorrect: true },
+            { answerText: 'Jardines Colgantes de Babilonia', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '¿Cuál de los siguientes peces es propio de alta mar?',
+        answerOptions: [
+            { answerText: 'Salmonete', isCorrect: false },
+            { answerText: 'Sardina', isCorrect: true },
+            { answerText: 'Lenguado', isCorrect: false },
+            { answerText: 'Mero', isCorrect: false },
+        ],
+    },
+];
